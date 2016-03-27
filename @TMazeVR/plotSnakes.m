4ec4bf919908245fedaab8acece7e0d957631d92
mkrumin <mkrumin@gmail.com>
2016-03-27 17:38:47 +0100

    plotSnakes.m goes multi-dataset
    Now the plots combine many datasets together, and include SF
    model-predicted data plots as well. Initial commit with these
    capabilities.

--- @TMazeVR/plotSnakes.m
+++ @TMazeVR/plotSnakes.m
@@ -1,87 +1,127 @@
-function plotSnakes(data)
+function plotSnakes(data, results)
 
 separateColormap = false;
 climPrctiles = [50 99];
+margin = 0.1;
 
 nDatasets = length(data);
 for iDataset = 1:nDatasets
+    fprintf('iDataset %d/%d\n', iDataset, nDatasets);
     obj = data(iDataset);
-% take only legal trials
-
-idxAll = find(ismember(obj.dataTMaze.report, 'LR'));
-nTrials = length(idxAll);
-% divide trials into groups (e.g. going L/R)
-
-idxL = obj.dataTMaze.report(idxAll) == 'L';
-idxR = obj.dataTMaze.report(idxAll) == 'R';
-idxC = obj.dataTMaze.outcome(idxAll) == 'C';
-idxW = obj.dataTMaze.outcome(idxAll) == 'W';
-
-% get traces with uniform z
-
-% [thVector, zVector, fVector, tVector] = buildVectors(obj, trialIdx, tData, fData)
-
-thVector = cell(0);
-zVector = cell(0);
-fVector = cell(0);
-% tVector = cell(0);
-zMin = [];
-zMax = [];
-
-getCoords = true; % this is a flag of the first pass (the first plane is analyzed)
-for iPlane = obj.Planes
-    cellIdx = cell2mat(obj.data2p{iPlane}.ROI.CellClasses) == 's';
-%     cellIdx = ismember(cell2mat(obj.data2p{iPlane}.ROI.CellClasses), 'sa');
-    fData = obj.data2p{iPlane}.F(:, cellIdx);
-    tData = obj.times2p{iPlane}';
-    for iTrial = 1:nTrials
+    res = results(iDataset).res;
+    % take only legal trials
+    
+    idxAll = find(ismember(obj.dataTMaze.report, 'LR'));
+    nTrials = length(idxAll);
+    % divide trials into groups (e.g. going L/R)
+    
+    idxL = obj.dataTMaze.report(idxAll) == 'L';
+    idxR = obj.dataTMaze.report(idxAll) == 'R';
+    idxC = obj.dataTMaze.outcome(idxAll) == 'C';
+    idxW = obj.dataTMaze.outcome(idxAll) == 'W';
+    
+    idxL = idxL & idxC;
+    idxR = idxR & idxC;
+    
+    % get traces with uniform z
+    
+    % [thVector, zVector, fVector, tVector] = buildVectors(obj, trialIdx, tData, fData)
+    
+    thVector = cell(nTrials, 1);
+    zVector = cell(nTrials, 1);
+    fVector = cell(nTrials, max(obj.Planes));
+    nSamples = zeros(nTrials, 1);
+    % tVector = cell(0);
+    zMin = [];
+    zMax = [];
+    
+    getCoords = true; % this is a flag of the first pass (the first plane is analyzed)
+    for iPlane = obj.Planes
+        fprintf('iPlane %d\n', iPlane);
+        cellIdx = cell2mat(obj.data2p{iPlane}.ROI.CellClasses) == 's';
+        %     cellIdx = ismember(cell2mat(obj.data2p{iPlane}.ROI.CellClasses), 'sa');
+        fData = obj.data2p{iPlane}.F(:, cellIdx);
+        tData = obj.times2p{iPlane}';
         if getCoords
-            [thVector{iTrial}, zVector{iTrial}, fVector{iTrial, iPlane}, ~] =...
-                buildVectors(obj, idxAll(iTrial), tData, fData);
-            if ~isempty(zVector{iTrial})
-                zMin(iTrial) = min(zVector{iTrial});
-                zMax(iTrial) = max(zVector{iTrial});
-            else
-                zMin(iTrial) = NaN;
-                zMax(iTrial) = NaN;
+            for iTrial = 1:nTrials
+                [thVector{iTrial}, zVector{iTrial}, fVector{iTrial, iPlane}, ~] =...
+                    buildVectors(obj, idxAll(iTrial), tData, fData);
+                if ~isempty(zVector{iTrial})
+                    zMin(iTrial) = min(zVector{iTrial});
+                    zMax(iTrial) = max(zVector{iTrial});
+                else
+                    zMin(iTrial) = NaN;
+                    zMax(iTrial) = NaN;
+                end
+                nSamples(iTrial) = length(zVector{iTrial});
             end
+            getCoords = false;
         else
-            [~, ~, fVector{iTrial, iPlane}, ~] =...
-                buildVectors(obj, idxAll(iTrial), tData, fData);
+            [~, ~, fVectorTmp, ~] =...
+                buildVectors(obj, idxAll, tData, fData);
+            fVector(:, iPlane) = mat2cell(fVectorTmp, nSamples, size(fVectorTmp, 2));
+        end
+        %             res{iPlane}
+        
+        fModelTmp{iPlane} = nan(sum(nSamples), sum(cellIdx));
+        cellNumbers = find(cellIdx);
+        allTh = cell2mat(thVector(:));
+        allZ = cell2mat(zVector(:));
+        for iCell = 1:length(cellNumbers)
+            map = res{iPlane}(cellNumbers(iCell)).zThetaMap;
+            zCoords = res{iPlane}(cellNumbers(iCell)).zThetaBinCentres{1};
+            thCoords = res{iPlane}(cellNumbers(iCell)).zThetaBinCentres{2};
+            fModelTmp{iPlane}(:, iCell) = interp2(thCoords, zCoords, map, allTh, allZ);
+        end
+        
+    end
+    
+    fModelTmp = fModelTmp(:)'; % make sure it is a row array of cells
+    fModel = cell(length(nSamples), size(fModelTmp, 2));
+    for iPlane = obj.Planes
+        fModel(:, iPlane) = mat2cell(fModelTmp{1, iPlane}, nSamples, size(fModelTmp{1, iPlane}, 2));
+    end
+    
+    for iTrial = 1:nTrials
+        fVector{iTrial} = cell2mat(fVector(iTrial, :));
+        fModel{iTrial} = cell2mat(fModel(iTrial, :));
+    end
+    fVector = fVector(:,1);
+    fModel = fModel(:,1);
+    
+    zMinLimit = max(zMin);
+    zMaxLimit = min(zMax);
+    
+    nBins = 50;
+    zEdges = linspace(zMinLimit, zMaxLimit, nBins+1);
+    zAxis = (zEdges(1:end-1)+zEdges(2:end))/2;
+    zEdges(end) = Inf;
+    
+    nCells = size(fVector{1}, 2);
+    
+    fTraces = nan(nBins, nCells, nTrials);
+    fTracesModel = nan(nBins, nCells, nTrials);
+    for iTrial = 1:nTrials
+        [~, ~, idx] = histcounts(zVector{iTrial}, zEdges);
+        for iBin = 1:nBins
+            tmp = mean(fVector{iTrial}(idx == iBin, :), 1);
+            if ~isempty(tmp)
+                fTraces(iBin, :, iTrial) = tmp;
+            end
+            tmp = mean(fModel{iTrial}(idx == iBin, :), 1);
+            if ~isempty(tmp)
+                fTracesModel(iBin, :, iTrial) = tmp;
+            end
         end
     end
-    getCoords = false;
-end
+    
+    meanAll{iDataset} = nanmean(fTraces, 3);
+    meanL{iDataset} = nanmean(fTraces(:,:,idxL), 3);
+    meanR{iDataset} = nanmean(fTraces(:,:,idxR), 3);
 
-for iTrial = 1:nTrials
-    fVector{iTrial} = cell2mat(fVector(iTrial, :));
-end
-fVector = fVector(:,1);
-
-zMinLimit = max(zMin);
-zMaxLimit = min(zMax);
-
-nBins = 50;
-zEdges = linspace(zMinLimit, zMaxLimit, nBins+1);
-zAxis = (zEdges(1:end-1)+zEdges(2:end))/2;
-zEdges(end) = Inf;
-
-nCells = size(fVector{1}, 2);
-
-fTraces = nan(nBins, nCells, nTrials);
-for iTrial = 1:nTrials
-    [~, ~, idx] = histcounts(zVector{iTrial}, zEdges);
-    for iBin = 1:nBins
-        tmp = mean(fVector{iTrial}(idx == iBin, :), 1);
-        if ~isempty(tmp)
-        fTraces(iBin, :, iTrial) = tmp;
-        end
-    end
-end
-
-meanAll{iDataset} = nanmean(fTraces, 3);
-meanL{iDataset} = nanmean(fTraces(:,:,idxL), 3);
-meanR{iDataset} = nanmean(fTraces(:,:,idxR), 3);
+    meanAllModel{iDataset} = nanmean(fTracesModel, 3);
+    meanLModel{iDataset} = nanmean(fTracesModel(:,:,idxL), 3);
+    meanRModel{iDataset} = nanmean(fTracesModel(:,:,idxR), 3);
 
 end % iDataset
 
@@ -93,17 +133,28 @@ fTracesMean = cell2mat(meanAll);
 meanL = cell2mat(meanL);
 meanR = cell2mat(meanR);
 nCells = size(fTracesMean, 2);
+fTracesMeanModel = cell2mat(meanAllModel);
+meanLModel = cell2mat(meanLModel);
+meanRModel = cell2mat(meanRModel);
+
 
 h = mean(diff(zAxis));
 p = 1/(1+h^3/0.6);
 [fTracesMean, p] = csaps(zAxis, fTracesMean', p, zAxis);
 % [sp, fTracesMean, p] = spaps(zAxis, fTracesMean', 10.97);
 fTracesMean = fTracesMean';
+[fTracesMeanModel, p] = csaps(zAxis, fTracesMeanModel', p, zAxis);
+fTracesMeanModel = fTracesMeanModel';
 
 minValues = nanmin(fTracesMean);
 fTracesMean = bsxfun(@minus, fTracesMean, minValues);
 maxValues = nanmax(fTracesMean);
 fTracesMean = bsxfun(@rdivide, fTracesMean, maxValues);
+
+minValues = nanmin(fTracesMeanModel);
+fTracesMeanModel = bsxfun(@minus, fTracesMeanModel, minValues);
+maxValues = nanmax(fTracesMeanModel);
+fTracesMeanModel = bsxfun(@rdivide, fTracesMeanModel, maxValues);
 
 [~, latency] = max(fTracesMean);
 [~, order] = sort(latency);
@@ -121,17 +172,27 @@ ylabel('Cell #');
 colorbar
 
 subplot(2, 2, 2)
-plot(zAxis, fTracesMean(:, order))
+% plot(zAxis, fTracesMean(:, order))
+imagesc(zAxis, 1:nCells, fTracesMeanModel(:, order)');
+caxis(prctile(fTracesMean(:), climPrctiles))
+
+axis xy
+xlabel('z [cm]')
+ylabel('Cell #');
+colorbar
 
 meanL = csaps(zAxis, meanL', p, zAxis)';
 meanR = csaps(zAxis, meanR', p, zAxis)';
 
+meanLModel = csaps(zAxis, meanLModel', p, zAxis)';
+meanRModel = csaps(zAxis, meanRModel', p, zAxis)';
+
 % divide cells into two groups, based on the maximum F
-prefR = find(max(meanR)>(max(meanL)+0.0));
-prefL = find(max(meanL)>(max(meanR)+0.0));
+prefR = find(max(meanR)>(max(meanL)+margin));
+prefL = find(max(meanL)>(max(meanR)+margin));
 % OR, divide cells into two groups based on the mean F
-% prefR = find(mean(meanR)-mean(meanL)>0.1);
-% prefL = find(mean(meanR)-mean(meanL)<-0.1);
+% prefR = find(mean(meanR)-mean(meanL)>margin);
+% prefL = find(mean(meanR)-mean(meanL)<-margin);
 
 minValues(prefR) = nanmin(meanR(:, prefR));
 minValues(prefL) = nanmin(meanL(:, prefL));
@@ -141,6 +202,15 @@ maxValues(prefR) = nanmax(meanR(:, prefR));
 maxValues(prefL) = nanmax(meanL(:, prefL));
 meanL = bsxfun(@rdivide, meanL, maxValues);
 meanR = bsxfun(@rdivide, meanR, maxValues);
+
+minValues(prefR) = nanmin(meanRModel(:, prefR));
+minValues(prefL) = nanmin(meanLModel(:, prefL));
+meanLModel = bsxfun(@minus, meanLModel, minValues);
+meanRModel = bsxfun(@minus, meanRModel, minValues);
+maxValues(prefR) = nanmax(meanRModel(:, prefR));
+maxValues(prefL) = nanmax(meanLModel(:, prefL));
+meanLModel = bsxfun(@rdivide, meanLModel, maxValues);
+meanRModel = bsxfun(@rdivide, meanRModel, maxValues);
 
 
 nCellsL = numel(prefL);
@@ -197,6 +267,14 @@ colorbar
 [~, latency] = max(meanR(:, prefR));
 [~, orderPrefR] = sort(latency);
 
+% control ordering - L cells by their R response latency, and vice-versa
+
+% [~, latency] = max(meanR(:, prefL));
+% [~, orderPrefL] = sort(latency);
+% 
+% [~, latency] = max(meanL(:, prefR));
+% [~, orderPrefR] = sort(latency);
+
 subplot(4, 4, 11)
 imagesc(zAxis, 1:nCellsL, meanL(:, prefL(orderPrefL))');
 caxis(climsL);
@@ -216,12 +294,46 @@ subplot(4, 4, 15)
 imagesc(zAxis, 1:nCellsR, meanL(:, prefR(orderPrefR))');
 caxis(climsR);
 ylabel('Cells prefer Right')
+xlabel('z [cm]');
 axis xy
 colorbar;
 
 subplot(4, 4, 16)
 imagesc(zAxis, 1:nCellsR, meanR(:, prefR(orderPrefR))');
 caxis(climsR);
+xlabel('z [cm]');
+axis xy
+colorbar
+
+% plotting the SF model-predicted 'snakes'
+
+subplot(4, 4, 3)
+imagesc(zAxis, 1:nCellsL, meanLModel(:, prefL(orderPrefL))');
+caxis(climsL);
+title('Left trials');
+ylabel('Cells Prefer Left');
+axis xy
+colorbar;
+
+subplot(4, 4, 4)
+imagesc(zAxis, 1:nCellsL, meanRModel(:, prefL(orderPrefL))');
+caxis(climsL);
+title('Right trials');
+axis xy
+colorbar
+
+subplot(4, 4, 7)
+imagesc(zAxis, 1:nCellsR, meanLModel(:, prefR(orderPrefR))');
+caxis(climsR);
+ylabel('Cells prefer Right')
+xlabel('z [cm]');
+axis xy
+colorbar;
+
+subplot(4, 4, 8)
+imagesc(zAxis, 1:nCellsR, meanRModel(:, prefR(orderPrefR))');
+caxis(climsR);
+xlabel('z [cm]');
 axis xy
 colorbar
 
