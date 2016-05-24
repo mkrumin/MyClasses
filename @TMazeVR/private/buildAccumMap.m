3c22bb74cdb7941f8f78f1bafa4860e216159749
mkrumin <mkrumin@gmail.com>
2016-05-24 11:03:57 +0100

    Introduction of z-d maps
    First model - f(z,d)

--- @TMazeVR/private/buildAccumMap.m
+++ @TMazeVR/private/buildAccumMap.m
@@ -8,50 +8,13 @@ for iDim=1:nDims
 end
 % this is a ridiculously fast and not very intutive way to build the maps
 % look into the help of sparse to understand how it works
-accMap = full(sparse(cc(:,1), cc(:,2), signal, nBins(1), nBins(2)));
+switch nDims
+    case 2
+        accMap = full(sparse(cc(:,1), cc(:,2), signal, nBins(1), nBins(2)));
+    case 1
+        accMap = full(sparse(cc, ones(size(cc)), signal, nBins(1), 1));
+    otherwise
+        error('Only 1D and 2D maps supported for now');
+end
 
 return;
-
-% this code is about 100 times slower... (even after major optimization effort)
-
-[nSamples, nDims] = size(coords);
-indices = cell(nDims, 1);
-nBins = nan(1, nDims);
-% first, lets build indices matrices
-for iDim = 1:nDims
-    nBins(iDim) = length(binEdges{iDim})-1;
-    indices{iDim} = false(nSamples, nBins(iDim));
-    for iBin = 1:nBins(iDim)
-        indices{iDim}(:, iBin) = ...
-            coords(:, iDim)>=binEdges{iDim}(iBin) & ...
-            coords(:, iDim)<binEdges{iDim}(iBin+1);
-    end
-end
-accMap = nan(nBins);
-
-% trying to be independent of the nDims (not to hard-code the
-% dimensionality)
-str = '[sub(1)';
-for iDim = 2:nDims
-    str = sprintf('%s, sub(%d)', str, iDim);
-end
-str = [str, '] = ind2sub(nBins, iElement);'];
-
-nElements = prod(nBins);
-for iElement = 1:nElements
-    if nDims == 1
-        accMap(iElement) = sum(signal(indices{1}(:, iElement)));
-    elseif nDims ==2
-        [sub(1), sub(2)] = ind2sub(nBins, iElement);
-        accMap(iElement) = sum(signal(indices{1}(:, sub(1)) & indices{2}(:, sub(2))));
-    else
-        eval(str);
-        ind = indices{1}(:, sub(1));
-        for iDim=2:length(sub);
-            ind = ind & indices{iDim}(:, sub(iDim));
-        end
-        accMap(iElement) = sum(signal(ind));
-    end
-end
-
-end % buildAccumMap()
--- @TMazeVR/private/buildOccupMap.m
+++ @TMazeVR/private/buildOccupMap.m
@@ -1,9 +1,19 @@
 function [occMap, binCentres] = buildOccupMap(coords, binEdges)
 
-[occMap, binCentres] = hist3(coords, 'Edges', binEdges);
-occMap = occMap(1:end-1, 1:end-1);
-binCentres{1} = binCentres{1}(1:end-1);
-binCentres{2} = binCentres{2}(1:end-1);
+nDims = length(binEdges);
+switch nDims
+    case 2
+        [occMap, binCentres] = hist3(coords, 'Edges', binEdges);
+        occMap = occMap(1:end-1, 1:end-1);
+        binCentres{1} = binCentres{1}(1:end-1);
+        binCentres{2} = binCentres{2}(1:end-1);
+    case 1
+        [occMap, ~] = histcounts(coords, binEdges{1});
+        binCentres{1} = (binEdges{1}(1:end-1) + binEdges{1}(2:end))/2;
+    otherwise
+        error('buildOccupMap() only supports 1-D and 2-D maps');
+        
+end
 
 end % buildOccupMap();
 
