50a2f255c2522b616bc27cf4f4c8dbcb0aefd503
mkrumin <mkrumin@gmail.com>
2016-06-02 20:57:17 +0100

    Backup/sync commit

--- @TMazeVR/trainMapSFZD_CV.m
+++ @TMazeVR/trainMapSFZD_CV.m
@@ -1,4 +1,4 @@
-function [zthMap, zdMap, zthErr, zdErr, binCntrs] = trainMapSFZD_CV(obj, iPlane, iROI, options)
+function [zthErr, zthdErr, zdErr] = trainMapSFZD_CV(obj, iPlane, iROI, options)
 
 if nargin<4
     options = struct;
@@ -46,8 +46,10 @@ thVector = cell(nChunks, 1);
 zVector = cell(nChunks, 1);
 fVector = cell(nChunks, 1);
 zR = cell(nChunks, 1);
+thR = cell(nChunks, 1);
 fR = cell(nChunks, 1);
 zL = cell(nChunks, 1);
+thL = cell(nChunks, 1);
 fL = cell(nChunks, 1);
 for iChunk = 1:nChunks
     idxL = matrixL(iChunk, :)';
@@ -59,29 +61,44 @@ for iChunk = 1:nChunks
     zVector{iChunk} = cell2mat(zAll(trialIdx));
     fVector{iChunk} = cell2mat(fAll(trialIdx));
     zR{iChunk} = cell2mat(zAll(idxR));
+    thR{iChunk} = cell2mat(thAll(idxR));
     fR{iChunk} = cell2mat(fAll(idxR));
     zL{iChunk} = cell2mat(zAll(idxL));
+    thL{iChunk} = cell2mat(thAll(idxL));
     fL{iChunk} = cell2mat(fAll(idxL));
 end
 
 coords = cell(nChunks, 1);
+coordsR = cell(nChunks, 1);
+coordsL = cell(nChunks, 1);
 % estimating the optimal filter STDs
 for iChunk = 1:nChunks
     coords{iChunk} = [zVector{iChunk}, thVector{iChunk}];
+    coordsR{iChunk} = [zR{iChunk}, thR{iChunk}];
+    coordsL{iChunk} = [zL{iChunk}, thL{iChunk}];
 end
 [optStd, errVals, errValMatrix, stdGridValues] = ...
     estOptimalStd(coords, fVector, {zEdges, thEdges}, options.errPower);
+[optStdR3D, errValR3D, ~, ~] = ...
+    estOptimalStd(coordsR, fR, {zEdges, thEdges}, options.errPower);
+[optStdL3D, errValL3D, ~, ~] = ...
+    estOptimalStd(coordsL, fL, {zEdges, thEdges}, options.errPower);
 [optStdR, errValR, ~, ~] = ...
     estOptimalStd1D(zR, fR, {zEdges}, options.errPower);
 [optStdL, errValL, ~, ~] = ...
     estOptimalStd1D(zL, fL, {zEdges}, options.errPower);
 
-hFilter = ndGaussian(optStd);
-[zthMap, binCntrs] = estMap(cell2mat(coords), cell2mat(fVector), {zEdges, thEdges}, hFilter);
+[optStd25D, errVals25D, ~, ~] = ...
+    estOptimalStd25D([coordsL, coordsR], [fL, fR], {zEdges, thEdges}, options.errPower);
+
+% hFilter = ndGaussian(optStd);
+% [zthMap, binCntrs] = estMap(cell2mat(coords), cell2mat(fVector), {zEdges, thEdges}, hFilter);
+nR = length(validRTrials);
+nL = length(validLTrials);
 zthErr = errVals;
-zdErr = (errValL*length(validLTrials) + errValR*length(validRTrials))/...
-    (length(validLTrials) + length(validRTrials));
-zdMap = [];
+zthdErr = (errValL3D*nL + errValR3D*nR)/(nL + nR);
+zdErr = (errValL*nL + errValR*nR)/(nL+nR);
+
 
 end % TrainMaps()
 
@@ -367,3 +384,154 @@ end
 errValue = nanmean(chunkErrors); % mean() works better, maps are smoother and nicer, "outliers have less effect"
 
 end % mapErrorFaster()
+
+%==========================================================================
+function [stdOut, errVal, errValMatrix, gridValues] = estOptimalStd25D(coords2D, signal2D, binEdges, errPow)
+
+% coords = coords(:);
+% signal = signal(:);
+[nTrials, nSubsets] = size(coords2D);
+coords = cell(nTrials, 1);
+signal = cell(nTrials, 1);
+for iTrial = 1:nTrials
+    coords{iTrial, 1} = cat(1, coords2D{iTrial, 1}, coords2D{iTrial, 2});
+    signal{iTrial, 1} = cat(1, signal2D{iTrial, 1}, signal2D{iTrial, 2});
+end
+nDims = size(coords{1}, 2);
+
+% precalculate the occupancy and signal maps (unsmoothed - filter will be
+% our optimization parameter),
+% also linear indices of the testCoordinates
+nRows = length(binEdges{1})-1;
+nColumns = length(binEdges{2})-1;
+occMaps = zeros(nRows, nColumns, nTrials);
+fMaps = zeros(nRows, nColumns, nTrials);
+testXYLinearIdx = cell(nTrials, nSubsets);
+for iTrial = 1:nTrials
+    if isempty(coords{iTrial})
+        continue;
+    end
+    
+    [occMaps(:,:,iTrial), binCentres] = buildOccupMap(coords{iTrial}, binEdges);
+    fMaps(:,:,iTrial) = buildAccumMap(coords{iTrial}, signal{iTrial}, binEdges);
+    for iSubset = 1:nSubsets
+        ind = nan(size(coords2D{iTrial, iSubset}));
+        [~, ind(:, 1)] = histc(coords2D{iTrial, iSubset}(:,1), binEdges{1});
+        [~, ind(:, 2)] = histc(coords2D{iTrial, iSubset}(:,2), binEdges{2});
+        testXYLinearIdx{iTrial, iSubset} = sub2ind([nRows, nColumns], ind(:,1), ind(:,2));
+    end
+end
+% these are training sets (XOR because they exclude trial #1 data)
+occMapsXOR = zeros(size(occMaps));
+fMapsXOR = zeros(size(fMaps));
+for iTrial = 1:nTrials
+    idx = setdiff(1:nTrials, iTrial);
+    occMapsXOR(:,:,iTrial) = sum(occMaps(:,:,idx), 3);
+    fMapsXOR(:,:,iTrial) = sum(fMaps(:,:,idx), 3);
+end
+
+% let's run a grid search for the optimal parameters
+
+sz = size(occMaps);
+gridValues = cell(nDims, 1);
+for iDim = 1:nDims
+    minValue = 1;
+    maxValue = sz(iDim);
+    pp = 3;
+    gridValues{iDim} = linspace(minValue^(1/pp), maxValue^(1/pp), 15).^pp;
+end
+
+if nDims == 2
+    errValMatrix = nan(length(gridValues{1}), length(gridValues{2}));
+    for nInd = 1:length(gridValues{2})
+        % prefilter along the 2nd dimension
+        x2 = gridValues{2}(nInd);
+        hGauss2 = ndGaussian(x2);
+        
+        meanVal = squeeze(sum(sum(fMapsXOR), 2)./sum(sum(occMapsXOR), 2));
+        
+        occMapsXORSpecial = permute(occMapsXOR, [2, 1, 3]);
+        occMapsXORSpecial = reshape(occMapsXORSpecial, nColumns, nRows*nTrials);
+        occMapsXORFilt = conv2(hGauss2, 1, occMapsXORSpecial, 'same');
+        occMapsXORFilt = reshape(occMapsXORFilt, nColumns, nRows, nTrials);
+        occMapsXORFilt = permute(occMapsXORFilt, [2, 1, 3]);
+        
+        fMapsXORSpecial = permute(fMapsXOR, [2, 1, 3]);
+        fMapsXORSpecial = reshape(fMapsXORSpecial, nColumns, nRows*nTrials);
+        fMapsXORFilt = conv2(hGauss2, 1, fMapsXORSpecial, 'same');
+        fMapsXORFilt = reshape(fMapsXORFilt, nColumns, nRows, nTrials);
+        fMapsXORFilt = permute(fMapsXORFilt, [2, 1, 3]);
+        
+        for mInd = 1:length(gridValues{1})
+            x1 = gridValues{1}(mInd);
+            errValMatrix(mInd, nInd) = ...
+                mapError25D(x1, occMapsXORFilt, fMapsXORFilt, meanVal, testXYLinearIdx, signal2D, errPow);
+        end
+    end
+else
+    fprint('For now, grid search is implemented only for 2D grids\n');
+end
+
+ind = find(errValMatrix == min(errValMatrix(:)));
+[mOpt, nOpt] = ind2sub(size(errValMatrix), ind);
+stdOut = [gridValues{1}(mOpt), gridValues{2}(nOpt)];
+errVal = errValMatrix(ind);
+
+% % now run the optimization
+% initStd = 5*ones(1, nDims);
+% [stdOut, errVal] = fminsearch(@(x) mapError(x, occMaps, fMaps, testXYLlinIdx, testSignal), initStd);
+
+end % estOptimalStd()
+
+%==========================================================================
+function errValue = mapError25D(x1, occMaps, fMaps, meanF, testXYIdx, testF, errPow)
+
+x1(x1<0.2) = 0.2; % HACK! This value is in pixels, not real units
+
+hGauss = ndGaussian(x1);
+
+[nRows, nColumns, nTrials] = size(occMaps);
+chunkErrors = nan(nTrials, 1);
+
+epsilon = 0.01;
+meanFMatrix = repmat(permute(meanF, [2, 3, 1]), nRows, nColumns, 1);
+% allPredictionMaps = (imfilter(fMaps, hGauss)+epsilon*meanFMatrix)./(imfilter(occMaps, hGauss)+epsilon);
+fMapsSpecial = reshape(fMaps, nRows, []);
+occMapsSpecial = reshape(occMaps, nRows, []);
+meanFSpecial = reshape(meanFMatrix, nRows, []);
+allPredictionMaps = (conv2(hGauss, 1, fMapsSpecial, 'same')+epsilon*meanFSpecial)./(conv2(hGauss, 1, occMapsSpecial, 'same')+epsilon);
+allPredictionMaps = reshape(allPredictionMaps, nRows, nColumns, nTrials);
+
+for iTrial = 1:nTrials
+    if isempty(testF{iTrial})
+        % there is no data for this trial
+        continue;
+    end
+    %     occM = occMaps(:,:,iTrial);
+    %     fM = fMaps(:,:,iTrial);
+    %     predictionMap = filterAndDivideMaps1D(occM, fM, meanF(iTrial), hGauss);
+    predictionMap = allPredictionMaps(:,:,iTrial);
+    
+    predictedF{1} = predictionMap(testXYIdx{iTrial, 1});
+    predictedF{2} = predictionMap(testXYIdx{iTrial, 2});
+    
+    
+    
+    %     chunkErrors(iChunk) = var(predictedF - testF{iChunk})/var(testF{iChunk});
+    meanFTraining = meanF(iTrial);
+    % the idea here is to check how much better the map is relative to just
+    % using mean F map.
+    switch errPow
+        case 1
+            chunkErrors(iTrial) = sum(abs(predictedF - testF{iTrial}))/sum(abs(meanFTraining - testF{iTrial}));
+        case 2
+            chunkErrors(iTrial) = sum((predictedF - testF{iTrial}).^2)/sum((meanFTraining - testF{iTrial}).^2);
+        otherwise
+            chunkErrors(iTrial) = sum(abs(predictedF - testF{iTrial}).^errPow)/sum(abs(meanFTraining - testF{iTrial}).^errPow);
+    end
+end
+% errValue = nanmedian(chunkErrors);
+errValue = nanmean(chunkErrors); % mean() works better, maps are smoother and nicer, "outliers have less effect"
+
+end % mapErrorFaster()
+
