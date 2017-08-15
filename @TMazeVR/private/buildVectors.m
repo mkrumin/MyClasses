3e1abd340d14364cb89968a45c64c8c96844ade9
Julie <julie.lee.15@ucl.ac.uk>
2017-08-15 10:11:45 +0100

    merged VR and Replay buildVectors

--- @TMazeVR/private/buildVectors.m
+++ @TMazeVR/private/buildVectors.m
@@ -1,40 +1,42 @@
-function [thVector, zVector, fVector, tVector] = buildVectors(obj, trialIdx, tData, fData)
+function [thVector, zVector, fVector, tVector] = buildVectors(obj, trialIdx, tData, fData, isReplay)
+
+%builds vector for defined trial, if trial is scrambled then unscrambles
+%only takes argument of single trial idx not collection of trial idxs (yet)
 
 [~, zInd] = intersect(obj.dataTMaze.SESSION.allTrials(1).pospars, 'Z');
 [~, thInd] = intersect(obj.dataTMaze.SESSION.allTrials(1).pospars, 'theta');
 
-nSamples = 0;
-for trialNum = 1:length(trialIdx)
-    iTrial = trialIdx(trialNum);
-    if ~isempty(obj.timesVRframes(iTrial).idx)
-        nSamples = nSamples + length(obj.timesVRframes(iTrial).idx)-1;
-    end
-end
+if nargin < 5
+    isReplay = false;
+end 
 
-% pre-allocating
-thVector = nan(nSamples, 1);
-zVector = nan(nSamples, 1);
-fVector = nan(nSamples, size(fData, 2));
-tVector = nan(nSamples, 1);
-
-nSamplesAccum = 0;
-for trialNum = 1:length(trialIdx)
-    iTrial = trialIdx(trialNum);
-    tt = obj.timesVRframes(iTrial).t;
-    if isempty(tt)
-        continue;
-    end
-    idx = obj.timesVRframes(iTrial).idx(2:end);
+%calculate number of frames in trial
+if ~isReplay
+    posdata = obj.dataTMaze.SESSION.allTrials(trialIdx).posdata;
+    
+    tt = obj.timesVRframes(trialIdx).t;
     tt = tt(2:end-1);
-    nSamplesThisTrial = length(tt);
-    sampleIdx = nSamplesAccum+1:nSamplesAccum+nSamplesThisTrial;
-    thVector(sampleIdx) = obj.dataTMaze.SESSION.allTrials(iTrial).posdata(idx, thInd);
-    zVector(sampleIdx) = -obj.dataTMaze.SESSION.allTrials(iTrial).posdata(idx, zInd);
-    fVector(sampleIdx, :) = interp1(tData, fData, tt);
-    tVector(sampleIdx) = tt;
-    nSamplesAccum = nSamplesAccum + nSamplesThisTrial;
+elseif isReplay
+    snippets = obj.dataTMaze.SESSION.replaySnippets(:);
+    snippetIdx = [snippets.trialIndex]';
+    snippets2Use = find(ismember(snippetIdx, trialIdx));
+    nSamplesPerSnippet = size(snippets(1).posdata, 1);
+    
+    posdata = cell2mat({snippets(snippets2Use).posdata}');
+    
+    tSamples = bsxfun(@plus, [1:nSamplesPerSnippet]', nSamplesPerSnippet*(snippets2Use(:)'-1));
+    tSamples = tSamples(:);
+    tt = obj.timesVRframes{1}(tSamples);
 end
+
+idx = obj.timesVRframes(trialIdx).idx(2:end); %why is first one always 1?
+
+thVector = posdata(idx, thInd);
+zVector = -posdata(idx, zInd);
+
+fVector = interp1(tData, fData, tt);
+tVector = tt;
 
 thVector = thVector*180/pi; % transform from radians to degrees
 
-end % buildVectors()
+end 
